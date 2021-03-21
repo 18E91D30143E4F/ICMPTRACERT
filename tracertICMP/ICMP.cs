0bdcad8228cb9128371d545ba849aa7475235f21
beltyukov.e.danil@gmail.com <beltyukov.e.danil@gmail.com>
2021-03-22 00:05:58 +0300

    Добавьте файлы проекта.

---
+++ tracertICMP/ICMP.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tracertICMP
+{
+    public class ICMP
+    {
+        public byte Type { get; protected set; } = 0x08;
+        public byte Code { get; protected set; } = 0x00;
+        public int MessageSize { get; protected set; }
+        public int PacketSize { get; protected set; }
+
+        public byte[] Message = new byte[1024];
+        private UInt16 SequenceNumber = 107;
+        private byte[] Data;
+
+        public UInt16 Checksum = 0;
+
+        public ICMP(byte[] data)
+        {
+            this.Data = data;
+            MessageSize = data.Length + 4;
+            PacketSize = MessageSize + 4;
+            Message = createPacket();
+        }
+
+        public UInt16 UpdateSequence()
+        {
+            SequenceNumber++;
+            Message = createPacket();
+
+            return SequenceNumber;
+        }
+
+        private byte[] createPacket()
+        {
+            byte[] Packet = new byte[MessageSize + 9];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(Type), 0, Packet, 0, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(Code), 0, Packet, 1, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(1), 0, Packet, 4, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(SequenceNumber), 0, Packet, 7, 2);
+            Buffer.BlockCopy(Data, 0, Packet, 8, Data.Length);
+            Buffer.BlockCopy(BitConverter.GetBytes(getChecksum(Packet)), 0, Packet, 2, 2);
+
+             return Packet;
+        }
+
+        private UInt16 getChecksum(byte[] bytes)
+        {
+            UInt32 chcksm = 0;
+            int packetsize = MessageSize + 8;
+
+            for (int index = 0; index < packetsize; index += 2)
+                chcksm += Convert.ToUInt32(BitConverter.ToUInt16(bytes, index));
+
+            chcksm = (chcksm >> 16) + (chcksm & 0xffff);
+            chcksm += (chcksm >> 16);
+
+            return (UInt16)(~chcksm);
+        }
+    }
+}
---
+++ tracertICMP/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tracertICMP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MaxHop = 30;
+            bool needNames = true;
+            string strHost = "";
+            byte[] buffer = new byte[1024];
+            byte[] data = new byte[64]; Array.Clear(data, 0, data.Length);
+
+
+            if (args.Length > 0)
+                strHost = args[0];
+
+            if (args.Length > 1)
+                needNames = !(args[1] == "-p");
+
+            var myIcmp = new ICMP(data);
+
+            IPHostEntry iphe = Dns.GetHostEntry(strHost);
+
+            Socket host = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            IPEndPoint iep = new IPEndPoint(iphe.AddressList[0], 0);
+            EndPoint ep = (EndPoint)iep;
+            host.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 10000);
+
+            int recv, responceTimeout;
+            TimeSpan timestop;  DateTime timestart;
+
+            Console.WriteLine("Трассировка маршрута к {0} [{1}]\nс максимальным числом прыжков {2}:\n", strHost, iphe.AddressList[0], MaxHop);
+
+            for (int i = 1; i < MaxHop; i++)
+            {
+                host.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.IpTimeToLive, i);
+
+                string ipNotPort = "NF";
+                responceTimeout = 0;
+
+                Console.Write("{0, 3} ", i);
+
+                for (int j = 0; j < 3; j++)
+                {
+                    try
+                    {
+                        timestart = DateTime.Now;
+                            host.SendTo(myIcmp.Message, myIcmp.PacketSize, SocketFlags.None, iep);
+                            recv = host.ReceiveFrom(buffer, ref ep);
+                        timestop = DateTime.Now - timestart;
+
+                        ipNotPort = ep.ToString().Substring(0, ep.ToString().LastIndexOf(':'));
+
+                        Console.Write("{0, 5} ms  ", timestop.Milliseconds.ToString());
+                    }
+                    catch (SocketException)
+                    {
+                        responceTimeout++;
+                        Console.Write("{0, 10}", '*');
+                    }
+
+                    // Inc sequence
+                    myIcmp.UpdateSequence();               
+                }
+                if (needNames)
+                {
+                    try
+                    {
+                        IPHostEntry name = Dns.GetHostEntry(ipNotPort);
+                        Console.WriteLine(string.Format("{1} [{0}]", ipNotPort, name.HostName));
+                    }
+                    catch (SocketException)
+                    {
+                        if (responceTimeout == 3)
+                        {
+                            Console.WriteLine("Timeout");
+                        }
+                        else
+                        {
+                            Console.WriteLine(string.Format("{0} ", ipNotPort));
+                        }
+                    }
+                }
+                else
+                {
+                    if (responceTimeout == 3)
+                    {
+                        Console.WriteLine("Timeout");
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format("{0} ", ipNotPort));
+                    }
+                }
+
+                // isResponce code = 0
+                if (buffer[20] == 0)
+                {
+                    Console.WriteLine("\nТрассировка завершена");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
